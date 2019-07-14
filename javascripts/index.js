8ae11df17e1a335dd5bfc7c46532b758f8776aca
luvai <luvaihassanali@gmail.com>
2019-07-13 22:22:38 -0400

    add rest

---
+++ javascripts/index.js
@@ -0,0 +1,86 @@
+$(function () {
+    var isIE = window.ActiveXObject || "ActiveXObject" in window;
+    if (isIE) {
+        $('.modal').removeClass('fade');
+    }
+    //console.log("Ready");
+    // Get the modal
+    var modal = document.getElementById("myModal");
+    // Get the modal elements
+    var modalImg = document.getElementById("img01");
+    var captionText = document.getElementById("caption");
+    var lastCaption = "";
+    // Get image with jquery on click then insert it intothe modal (use its "alt" text as a caption)
+    $("img").click(function () {
+      modal.style.display = "block";
+      modalImg.src = this.src;
+      switch (this.id) {
+        case "0":
+          captionText.innerHTML = "• Windows Forms (C#) Application used to enable communication between Wii remote and car; Wiimote uses bluetooth human interface device profile, so it won’t work with HC-05 module since it uses the serial port profile<br>• Can be manually control using Wii remote motion sensors, so you can tilt the remote forward to accelerate, backwards to reverse, left and right for turning, or use A button for acceleration and B for reverse<br>• Added servo motor for precision control of front wheels and changed photoresistors from last version to tilt switch with barrier in front to detect obstacles lower than sensor view<br>• <a class='link' href='rccar2vid.html' target='_blank'>Video of car using manual control, switching to autocontrol, then back to manual</a><br>• <a class='link' href='https://github.com/luvaihassanali/wiimote_car' target='_blank'>GitHub</a><br>";
+          lastCaption = captionText.innerHTML;
+          break;
+        case "1":
+          modalImg.src = "./images/cnc-optimize-once.gif";
+          captionText.innerHTML = "• Constructed using Arduino Uno, 2 old DVD drives, stepper motor, and associated motor drivers (<a class='link' href='./images/cncfull.jpg' target='_blank'>high resolution</a>) <br>• Can draw images or text within a 4cm square area<br>• Uses <a class='link' href='https://en.wikipedia.org/wiki/G-code' target='_blank'>g-code</a>, which is code telling the motors what type of action to perform, produced from <a class='link' href='https://inkscape.org/' target='_blank'>Inkscape</a> and feeds input to Arduino through serial port using <a class='link' href='https://processing.org/' target='_blank'>Processing</a><br>• Video examples of plotting: <a class='link' href='may.html' target='_blank'>May 2019</a>, <a class='link' href='squidward.html' target='_blank'>Squidward</a>, and the <a class='link' href='globe.html' target='_blank'>Earth</a><br>• <a class='link' href='https://github.com/luvaihassanali/arduino_cnc' target='_blank'>GitHub</a><br>";
+          lastCaption = captionText.innerHTML;
+          break;
+        case "2":
+          modalImg.src = "./images/electron-app-optimize-thrice.gif";
+          captionText.innerHTML = "• Constructed using an old laptop running <a class='link' href='https://lubuntu.net/' target='_blank'>lubuntu</a> and <a class='link' href='https://electronjs.org/' target='_blank'>Electron</a> application: open-source framework developed by GitHub to create cross platform desktop applications<br>• Uses JavaScript to query <a class='link' href='http://www.octranspo.com/en/plan-your-trip/travel-tools/developers/' target='_blank'>OC Transpo</a> and <a class='link' href='https://openweathermap.org/api' target='_blank'>OpenWeatherMap</a> API every twenty seconds to update information<br>• Real-time radar built using Arduino which communicates distance of objects detected within 150cm through serial port<br>• Video of display without the radar running in the <a class='link' href='busday.html' target='_blank'> daytime</a> and <a class='link' href='busnight.html' target='_blank'> nighttime</a>";
+          lastCaption = captionText.innerHTML;
+          break;
+        case "3":
+          captionText.innerHTML = "Windows - <a class='link' href='https://github.com/luvaihassanali/c_sharp_passman' target='_blank'>GitHub</a><br>• Utilizes .NET Framework data protection API <a class='link' href='https://docs.microsoft.com/en-us/dotnet/api/system.security.cryptography.protecteddata?view=netframework-4.8' target='_blank'>(System.Security.Cryptography)</a> to secure passwords using information from the current user account<br>• Copies password to clipboard after decryption then application minimizes automatically to system tray and clipboard is cleared after 5 seconds<br>Linux - <a class='link' href='https://github.com/luvaihassanali/wxpassman' target='_blank'>GitHub</a><br>• Similar design to Windows version but created in C++ with <a class='link' href='https://www.wxwidgets.org/' target='_blank'>wxWidgets GUI library</a><br>• Uses <a class='link' href='https://www.cryptopp.com/' target='_blank'>Crypto++ library</a> to implement AES encryption with PBKDF2-derived master key and <a class='link' href='https://www.sqlite.org/index.html' target='_blank'>SQLite</a> for storage of passwords<br>";
+          lastCaption = captionText.innerHTML;
+          break;
+        case "4":
+          captionText.innerHTML = "• Semester long team project for Object-Oriented Software Eng. course taken at Carleton including weekly progress reports and team meetings<br>• Recreation of <a class='link' href='https://boardgamegeek.com/boardgame/360/quests-round-tableboard' target='_blank'>old board game</a> using <a class='link' href='https://spring.io/' target='_blank'>Spring framework</a> with two main iterations: developing the game and then implementing networking functionality<br>• Follows MVC pattern and uses WebSockets for communication<br>• <a class='link' href='https://github.com/luvaihassanali/springmvc/tree/master/GUIShots' target='_blank'>More screenshots</a><br>• <a class='link' href='https://github.com/luvaihassanali/springmvc' target='_blank'>GitHub</a><br>";
+          lastCaption = captionText.innerHTML;
+          break;
+        case "5":
+          captionText.innerHTML = "• Final project for Neural Networks course taken at Carleton<br>• Implemented using <a class='link' href='https://www.tensorflow.org/' target='_blank'>TensorFlow</a> and <a class='link' href='https://scikit-learn.org/stable/' target='_blank'>Scikit-learn library</a><br>• Uses images from <a class='link' href='http://vis-www.cs.umass.edu/lfw/' target='_blank'>Labeled Faces in the Wild dataset</a>, a collection of face photographs designed for facial recognition, where subject has at least 70 distinct photos<br>• <a class='link' href='https://github.com/luvaihassanali/tf_sklearn_facialrecog' target='_blank'>GitHub</a><br>";
+          lastCaption = captionText.innerHTML;
+          break;
+        case "6":
+          captionText.innerHTML = "• For Windows and Linux dual-boot setup: a note taking app for each system which lives in system tray and derives notes from a shared partition, so edits are synced<br>• Windows Forms App and C++ app which uses <a class='link' href='https://www.wxwidgets.org/' target='_blank'>wxWidgets GUI library</a> for Linux<br>• Reduced memory consumption in comparison to previously used appilcations<br>• Both versions save each time application loses focus to prevent loss of data<br>• <a class='link' href='https://github.com/luvaihassanali/listApp_windows' target='_blank'>Windows: GitHub</a>, <a class='link' href='https://github.com/luvaihassanali/listApp_linux' target='_blank'>Linux: GitHub</a><br>";
+          lastCaption = captionText.innerHTML;
+          break;
+        case "7":
+          captionText.innerHTML = "• Arduino Nano, HC-05 Bluetooth module, and servo motor<br>• Can connect using <a class='link' href='https://play.google.com/store/apps/details?id=Qwerty.BluetoothTerminal&hl=en_CA' target='_blank'>Bluetooth Terminal</a> App from Google Play on Android device or using <a class='link' href='https://linux.die.net/man/1/minicom' target='_blank'>minicom</a> on Linux<br>• Only caveat is that kettle must be placed in a certain position after use<br>• <a class='link' href='https://github.com/luvaihassanali/arduino_code/blob/master/bluetooth_kettle_switch.ino' target='_blank'>GitHub</a><br>";
+          lastCaption = captionText.innerHTML;
+          break;
+        case "8":
+          captionText.innerHTML = "• Arduino Uno, 2 DC motors, L239d motor driver, 2 photoresistors, servo motor, ultrasonic sensor to avoid obstacles and shaded regions<br>• Photoresistors tell car to back out of areas where regions are dimly lit to certain threshold (shaded regions under furniture mostly)<br>• Ultrasonic sensor attatched to servo motor detects objects in front of car by 20 cm then swivels to check surrounding area for next action<br>•<a class='link' href='./images/rccar_gif.gif' target='_blank'>Car in motion</a><br>•<a class='link' href='https://github.com/luvaihassanali/arduino_code/blob/master/rc_car.ino' target='_blank'>GitHub</a><br>";
+          lastCaption = captionText.innerHTML;//   
+          break;
+        case "9":
+          captionText.innerHTML = "• Application which provides user with recipes based on ingredients entered<br>• Uses <a class='link' href='https://expressjs.com/' target='_blank'>Express framework</a> and client-side JavaScript to manipulate page elements<br>• <a class='link' href='https://www.food2fork.com/about/api' target='_blank'>Food2Fork public API</a> provides functionality to query for recipes based on ingredients<br>• <a class='link' href='https://github.com/luvaihassanali/food2fork' target='_blank'>GitHub</a><br>";
+          lastCaption = captionText.innerHTML;
+          break;
+        case "10":
+          captionText.innerHTML = "• Google Chrome extension which replaces all &lt;img&gt; tags on HTML pages with a different image of <a class='link' href='https://en.wikipedia.org/wiki/Hugo_%22Hurley%22_Reyes' target='_blank'>Hugo Reyes</a> from LOST<br>• Written in JavaScript<br>• <a class='link' href='https://github.com/luvaihassanali/hugo_chrome_extension' target='_blank'>GitHub</a><br>"
+          lastCaption = captionText.innerHTML;
+          break;
+        case "img01":
+          captionText.innerHTML = lastCaption;
+        case "img02":
+          captionText.innerHTML = lastCaption;
+          modal.style.display = "none";
+        default:
+        //https://stackoverflow.com/questions/4649423/should-switch-statements-always-contain-a-default-clause
+      }
+    });
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+      modal.style.display = "none";
+    }
+    // Close window when user presses outside of modal
+    window.onclick = function (event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
+  });
+  
